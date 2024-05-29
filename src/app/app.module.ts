9578a91344ddc942f63fdba9d7a2fdac7e5da23f
Md Shahed Talukder <shahedtalukder51@gmail.com>
2024-05-29 10:27:03 +0600

    directives done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
 import { Test4Component } from './test4/test4.component';
 import { Test5Component } from './test5/test5.component';
+import { TwowayComponent } from './twoway/twoway.component';
+import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,13 @@ import { Test5Component } from './test5/test5.component';
     Test2Component,
     Test3Component,
     Test4Component,
-    Test5Component
+    Test5Component,
+    TwowayComponent,
+    DirectivesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
